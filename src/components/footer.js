90d334d954a03946bd7b5a3e39b7d104752a6299
PATRICK MALLERY <patrick@openfoodnetwork.org.uk>
2023-05-20 01:06:35 +0100

    update

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = () => {
+  return (
+    <footer className="border-2">
+          <div className="social-icons">
+          <FontAwesomeIcon icon="faCoffee" className="icon"/>
+          </div>
+
+    </footer>
+    )
+}
+      export default Footer;
